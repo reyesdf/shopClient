084a5d1dfc9d7b3e3e07c624fe9dbf62f6d7b588
reyesdf <reyes.dennisjoseph@gmail.com>
2022-02-17 10:21:14 +0800

    setup boilerplate for front end client, adding ChakraUI for styling

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
